c78d8cefa93238fdb1134030b60d9afc5a4b6e31
tomoki-yamamura <progtab17@gmail.com>
2024-06-23 04:44:26 +0800

    basic product page

---
+++ src/lib/formatters.ts
@@ -0,0 +1,26 @@
+const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
+  currency: "USD",
+  style: "currency",
+  minimumFractionDigits: 0,
+})
+
+export function formatCurrency(amount: number) {
+  return CURRENCY_FORMATTER.format(amount)
+}
+
+const NUMBER_FORMATTER = new Intl.NumberFormat("en-US")
+
+export function formatNumber(number: number) {
+  return NUMBER_FORMATTER.format(number)
+}
+
+const PERCENT_FORMATTER = new Intl.NumberFormat("en-US", { style: "percent" })
+
+const DATE_TIME_FORMATTER = new Intl.DateTimeFormat("en", {
+  dateStyle: "medium",
+  timeStyle: "short",
+})
+
+export function formatDateTime(date: Date) {
+  return DATE_TIME_FORMATTER.format(date)
+}
